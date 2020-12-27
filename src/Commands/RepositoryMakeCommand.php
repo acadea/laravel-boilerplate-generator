d7e31ed6df2e0b797fb9943ac223a9498d35eb7b
Sam <hc.ngu01@gmail.com>
2020-12-27 23:04:13 +0800

    repository maker now populating fillables

--- src/Commands/RepositoryMakeCommand.php
+++ src/Commands/RepositoryMakeCommand.php
@@ -2,6 +2,7 @@
 
 namespace Acadea\Boilerplate\Commands;
 
+use Acadea\Boilerplate\Utils\SchemaStructure;
 use Acadea\Fixer\Facade\Fixer;
 use Illuminate\Console\Command;
 use Illuminate\Console\GeneratorCommand;
@@ -61,12 +62,65 @@ class RepositoryMakeCommand extends GeneratorCommand
         return $rootNamespace.'\Repositories\Api\V1\\';
     }
 
+    protected function getSchemaFields()
+    {
+        $structure = SchemaStructure::get();
+
+        return data_get($structure, strtolower(Str::singular($this->modelVariable())));
+    }
+
+    public function replaceCreateFields()
+    {
+        $fields = $this->getSchemaFields();
+
+        return collect($fields)->map(function ($field, $fieldName){
+            if(data_get($field, 'type') === 'pivot'){
+                return '';
+            }
+            return '\'' . $fieldName . '\' => data_get($data, \'' . $fieldName . '\'),';
+        })->join("\n");
+    }
+
+    public function replaceUpdateFields()
+    {
+        $fields = $this->getSchemaFields();
+
+        return collect($fields)->map(function ($field, $fieldName){
+            if(data_get($field, 'type') === 'pivot'){
+                return '';
+            }
+            return '\'' . $fieldName . '\' => data_get($data, \'' . $fieldName . '\') ?? $' . $this->modelVariable() .'->' . $fieldName . ',';
+        })->join("\n");
+    }
+
+    public function replaceManyToManySync()
+    {
+        $fields = $this->getSchemaFields();
+        return collect($fields)
+            ->filter(function ($field){
+                return data_get($field, 'type') === 'pivot';
+            })
+            ->map(function ($field, $fieldName){
+                $foreignKey = Str::plural(data_get($field, 'pivot.foreign_key'));
+                return 'if(data_get($data, \'' . $foreignKey . '\')){' . "\n" .
+                    '$' . $this->modelVariable() . '->' . $fieldName . '()->sync(data_get($data, \'' . $foreignKey . '\'));' . "\n" .
+                    '}';
+            })
+            ->join("\n");
+    }
+
     public function buildClass($name)
     {
-        $replace = [];
+        $replace = [
+            '{{ createFields }}' => $this->replaceCreateFields(),
+            '{{createFields}}' => $this->replaceCreateFields(),
+            '{{ updateFields }}' => $this->replaceUpdateFields(),
+            '{{updateFields}}' => $this->replaceUpdateFields(),
+            '{{manyToManySync}}' => $this->replaceManyToManySync(),
+            '{{ manyToManySync }}' => $this->replaceManyToManySync(),
+        ];
 
-        $replace = $this->buildModelReplacements($replace);
-
+        $replace = array_merge($replace, $this->buildModelReplacements($replace));
         return Fixer::format(str_replace(
             array_keys($replace),
             array_values($replace),
@@ -103,6 +157,15 @@ class RepositoryMakeCommand extends GeneratorCommand
         return $this->option('model');
     }
 
+    protected function modelClass()
+    {
+        return $this->parseModel($this->getModelName());
+    }
+
+    protected function modelVariable()
+    {
+        return lcfirst(class_basename($this->modelClass()));
+    }
     /**
      * Build the model replacement values.
      *
@@ -111,11 +174,13 @@ class RepositoryMakeCommand extends GeneratorCommand
      */
     protected function buildModelReplacements(array $replace)
     {
-        $modelClass = $this->parseModel($this->getModelName());
+        $modelClass = $this->modelClass();
 
         if (! class_exists($modelClass)) {
             throw new ModelNotFoundException('Model does not exist. Namespace: ' . $modelClass);
         }
+
+        $modelVariable = $this->modelVariable();
 
         return array_merge($replace, [
             'DummyFullModelClass' => $modelClass,
@@ -124,9 +189,9 @@ class RepositoryMakeCommand extends GeneratorCommand
             'DummyModelClass' => class_basename($modelClass),
             '{{ model }}' => class_basename($modelClass),
             '{{model}}' => class_basename($modelClass),
-            'DummyModelVariable' => lcfirst(class_basename($modelClass)),
-            '{{ modelVariable }}' => lcfirst(class_basename($modelClass)),
-            '{{modelVariable}}' => lcfirst(class_basename($modelClass)),
+            'DummyModelVariable' => $modelVariable,
+            '{{ modelVariable }}' => $modelVariable,
+            '{{modelVariable}}' => $modelVariable,
         ]);
     }
 
---
+++ tests/Features/Repository/RepositoryTest.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace Acadea\Boilerplate\Tests\Features\Repository;
+
+
+use Acadea\Boilerplate\Tests\Helpers\StringHelper;
+use Acadea\Boilerplate\Tests\TestCase;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\File;
+
+class RepositoryTest extends TestCase
+{
+    // test controller generated is ok
+    public function setUp(): void
+    {
+        parent::setUp();
+        // generate a test controller based on fake model
+
+        Artisan::call('boilerplate:model', [
+            'name' => 'Post'
+        ]);
+        require $this->app->path('Models/Post.php');
+        Artisan::call('boilerplate:repository', [
+            'name' => 'PostRepository',
+        ]);
+
+        $this->beforeApplicationDestroyed(function (){
+//            File::delete($this->app->path('Repository/Api/V1/PostRepository.php'));
+        });
+    }
+
+    public function test_generated_controller_is_correct()
+    {
+        // get controller instance
+        $path = $this->app->path('Repositories/Api/V1/PostRepository.php');
+        $file = File::get($path);
+
+        $sourceOfTruth = File::get(self::TEST_ASSERT_FILES_PATH . '/PostRepository.php');
+        // verify generated file is the same as source of truth
+        $this->assertEquals(StringHelper::clean($sourceOfTruth), StringHelper::clean($file), 'not the same as known truth');
+    }
+}
